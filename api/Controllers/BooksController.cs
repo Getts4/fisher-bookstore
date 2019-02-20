e9f0d32c6bc7950da98787a535a91e7e334efb80
Brendan Getts <getts.4@buckeyemail.osu.edu>
2019-02-19 19:59:38 -0500

    Create Book with Post

--- api/Controllers/BooksController.cs
+++ api/Controllers/BooksController.cs
@@ -61,6 +61,18 @@ namespace Fisher.Bookstore.Api.Controllers
 
             return Ok(book);
         }
+        [HttpPost]
+        public IActionResult Post([FromBody]Book book)
+        {
+            if (book == null){
+                return BadRequest();
+            }
+
+            db.Books.Add(book);
+            db.SaveChanges();
+
+            return CreatedAtRoute("GetBook", new {id = book.Id}, book);
+        }
        
     }
 }
